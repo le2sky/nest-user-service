6ab47c9cdf589b6079f0177ee651fbcb45ddb57c
leesky <leehaneul0623@gmail.com>
2021-12-16 22:37:01 +0900

    [DTO issue, 커스텀 데코 추가]NotIn 커스템 validation 추가, DTO issue 발생

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -14,15 +14,20 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { NotIn } from 'src/util/decorators/validation/string/not-in.decorator';
 
 export class CreateUserDto {
-  @Transform(({ value, obj }) => {
-    if (obj.password.includes(value.trim())) {
-      throw new BadRequestException(
-        'password에 name과 같은 문자열을 포함할 수 없습니다.',
-      );
-    }
-    return value.trim();
+  // @Transform(({ value, obj }) => {
+  //   if (obj.password.includes(value.trim())) {
+  //     throw new BadRequestException(
+  //       'password에 name과 같은 문자열을 포함할 수 없습니다.',
+  //     );
+  //   }
+  //   return value.trim();
+  // })
+
+  @NotIn('password', {
+    message: 'password는 name과 같은 문자열을 포함할 수 없습니다.',
   })
   @IsString()
   @MinLength(2)
@@ -35,29 +40,30 @@ export class CreateUserDto {
   @MaxLength(60)
   public readonly email: string;
 
-  @Transform(({ value }: { value: string }) => {
-    [...value.trim()].forEach((item: string, index: number, arr: string[]) => {
-      if (+item && index + 3 <= arr.length) {
-        if (
-          !isNaN(+arr[index + 1]) &&
-          !isNaN(+arr[index + 2]) &&
-          !isNaN(+arr[index + 3])
-        ) {
-          console.log('hi');
-          let count = 0;
-          for (let i = index + 3; i > index; i--) {
-            count -= +arr[i] - +arr[i - 1];
-          }
-          if (count === -3 || count === +3)
-            throw new BadRequestException(
-              '비밀번호에 연속된 숫자를 입력할 수 없습니다!',
-            );
-        }
-      }
-    });
-    return value.trim();
-  })
+  // 이부분 다시 구현 필요 엣지 케이스 확인 + Internal server error 나는데
+  // 디버깅 필요할듯
+  // @Transform(({ value }: { value: string }) => {
+  //   [...value.trim()].forEach((item: string, index: number, arr: string[]) => {
+  //     if (+item && index + 3 <= arr.length) {
+  //       if (
+  //         !isNaN(+arr[index + 1]) &&
+  //         !isNaN(+arr[index + 2]) &&
+  //         !isNaN(+arr[index + 3])
+  //       ) {
+  //         let count = 0;
+  //         for (let i = index + 3; i > index; i--) {
+  //           count -= +arr[i] - +arr[i - 1];
+  //         }
+  //         if (count === -3 || count === +3)
+  //           throw new BadRequestException(
+  //             '비밀번호에 연속된 숫자를 입력할 수 없습니다!',
+  //           );
+  //       }
+  //     }
+  //   });
+  //   return value.trim();
+  // })
   @IsString()
-  @Matches(/^[A-Za-z\d!@#$%^&*()]{8, 30}$/)
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
   public readonly password: string;
 }
---
+++ src/util/decorators/validation/string/not-in.decorator.ts
@@ -0,0 +1,30 @@
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+} from 'class-validator';
+
+//검사할 대상에 target이 존재하는지 검사하는 기능을합니다.
+export function NotIn(property: string, validationOptions?: ValidationOptions) {
+  return (object: Object, propertyName: string) => {
+    console.log(object);
+    registerDecorator({
+      name: 'NotIn',
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      constraints: [property],
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+          const relatedValue = (args.object as any)[relatedPropertyName];
+          return (
+            typeof value === 'string' &&
+            typeof relatedValue === 'string' &&
+            !relatedValue.includes(value)
+          );
+        },
+      },
+    });
+  };
+}
